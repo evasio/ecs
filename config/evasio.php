b31638ae6820f4ea16b846b7c3916e0951491608
Filip Mikovcak <fmikovcak@gmail.com>
2023-12-12 00:09:44 +0100

    ECS: Add SetList::EVASIO

---
+++ config/evasio.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Basic\BracesFixer;
+use PhpCsFixer\Fixer\Basic\BracesPositionFixer;
+use PhpCsFixer\Fixer\Basic\CurlyBracesPositionFixer;
+use PhpCsFixer\Fixer\ClassNotation\NoBlankLinesAfterClassOpeningFixer;
+use PhpCsFixer\Fixer\Operator\NewWithBracesFixer;
+use PhpCsFixer\Fixer\Operator\NewWithParenthesesFixer;
+use SlevomatCodingStandard\Sniffs\Classes\EmptyLinesAroundClassBracesSniff;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+	$ecsConfig->indentation('tab');
+	$ecsConfig->lineEnding("\n");
+
+	$ecsConfig->sets([
+		SetList::ARRAY,
+		SetList::NAMESPACES,
+		SetList::COMMENTS,
+		SetList::STRICT,
+		SetList::PHPUNIT,
+		SetList::PSR_12,
+	]);
+
+	$ecsConfig->rules([
+		NewWithParenthesesFixer::class,
+		EmptyLinesAroundClassBracesSniff::class,
+	]);
+
+	$ecsConfig->skip([
+		BracesFixer::class,
+		NoBlankLinesAfterClassOpeningFixer::class,
+		NewWithBracesFixer::class,
+		CurlyBracesPositionFixer::class,
+		BracesPositionFixer::class,
+	]);
+};
---
+++ src/SetList.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Evasio\ECS;
+
+final class SetList
+{
+
+	public const EVASIO = __DIR__ . '/../config/evasio.php';
+
+}
